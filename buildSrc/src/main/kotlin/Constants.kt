fa438c7d0304786735025c0a307eb33c9529b517
Gian Quintana <gianpatrick27@gmail.com>
2021-06-05 21:15:14 +0800

    2.0.4.1

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,7 +1,7 @@
 object Constants {
 
     const val KOTLIN_VERSION: String = "1.5.10"
-    const val VERSION_CODE: Int = 204
-    const val VERSION_NAME: String = "2.0.3.1"
+    const val VERSION_CODE: Int = 205
+    const val VERSION_NAME: String = "2.0.4.1"
 
 }
