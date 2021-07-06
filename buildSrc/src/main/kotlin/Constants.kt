56139050b0c56fadbb62b33a967c9adc17e46994
Gian Patrick Quintana <gianpatrick27@gmail.com>
2021-07-06 10:32:45 +0800

    2.0.5-HK-1

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -2,6 +2,6 @@ object Constants {
 
     const val KOTLIN_VERSION: String = "1.5.10"
     const val VERSION_CODE: Int = 205
-    const val VERSION_NAME: String = "2.0.4.1"
+    const val VERSION_NAME: String = "2.0.5-HK.1"
 
 }
